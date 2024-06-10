966d44b6868393fabeb72f7cc57174796f19da1d
tusharsaini-05 <155976478+tusharsaini-05@users.noreply.github.com>
2024-06-10 14:00:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+
+
+
+
+
+
+
+
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+    
+}
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.toggle('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.toggle('active');
+    cartItem.classList.remove('active');
+    s
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+}
+
